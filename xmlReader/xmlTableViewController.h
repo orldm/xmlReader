57b901c7975d47d894ce7298fd38acc247e85d87
Dmitri <orldm@mail.ru>
2013-05-07 13:12:12 +0300

    Initial commit

---
+++ xmlReader/xmlTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  xmlTableViewController.h
+//  xmlReader
+//
+//  Created by John Smith on 5/4/13.
+//  Copyright (c) 2013 bullet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xmlTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *xmlFiles;
+
+@end
